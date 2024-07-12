0c394060b72cc0dda177f3c178573c0ad3cb9b1f
Olga Te <teteriog@gmail.com>
2024-07-12 14:50:25 +0200

    Add TodoService class with XUnit test

---
+++ Console core Tests/TodoServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using Console_core.Date;
+using Console_core.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Console_core_Tests
+{
+    public class TodoServiceTests
+    {
+        [Fact]
+        public void Size_ShouldReturnZero_WhenArrayIsEmpty()
+        {
+            // Arrange
+            TodoService todoService = new TodoService();
+            todoService.Clear();
+
+            // Act
+            int size = todoService.Size();
+
+            // Assert
+            Assert.Equal(0, size);
+        }
+
+        [Fact]
+        public void FindAll_ShouldReturnEmptyArray_WhenNoTodoItems()
+        {
+            // Arrange
+            TodoService todoService = new TodoService();
+            todoService.Clear();
+
+            // Act
+            Todo[] todos = todoService.FindAll();
+
+            // Assert
+            Assert.Empty(todos);
+        }
+
+        [Fact]
+        public void FindById_ShouldReturnCorrectTodoItem_WhenTodoItemExists()
+        {
+            // Arrange
+            TodoService todoService = new TodoService();
+            todoService.Clear();
+            Person assignee = new Person(PersonSequencer.NextPersonId(), "John", "Doe");
+            Todo todo = todoService.CreateTodoItem("Test Task", false, assignee);
+
+            // Act
+            Todo foundTodo = todoService.FindById(todo.Id);
+
+            // Assert
+            Assert.Equal(todo, foundTodo);
+        }
+
+        [Fact]
+        public void CreateTodoItem_ShouldAddTodoToArray()
+        {
+            // Arrange
+            TodoService todoService = new TodoService();
+            todoService.Clear();
+            int initialSize = todoService.Size();
+            Person assignee = new Person(PersonSequencer.NextPersonId(), "John", "Doe");
+
+            // Act
+            todoService.CreateTodoItem("Test Task", false, assignee);
+            int newSize = todoService.Size();
+
+            // Assert
+            Assert.Equal(initialSize + 1, newSize);
+        }
+
+        [Fact]
+        public void Clear_ShouldEmptyTheArray()
+        {
+            // Arrange
+            TodoService todoService = new TodoService();
+            Person assignee = new Person(PersonSequencer.NextPersonId(), "John", "Doe");
+            todoService.CreateTodoItem("Test Task", false, assignee);
+
+            // Act
+            todoService.Clear();
+            int size = todoService.Size();
+
+            // Assert
+            Assert.Equal(0, size);
+        }
+    }
+}
---
+++ Date/TodoService.cs
@@ -0,0 +1,41 @@
+﻿using Console_core.Models.Models;
+
+namespace Console_core.Date
+{
+    public class TodoService
+    {
+        private static Todo[] todoItems = new Todo[0];
+
+        public int Size()
+        {
+            return todoItems.Length;
+        }
+
+        public Todo[] FindAll()
+        {
+            return todoItems;
+        }
+
+        public Todo FindById(int todoId)
+        {
+            return todoItems.FirstOrDefault(todo => todo.Id == todoId);
+        }
+
+        public Todo CreateTodoItem(string description, bool done, Person assignee)
+        {
+            Todo newTodo = new Todo(TodoSequencer.NextTodoId(), description)
+            {
+                Done = done,
+                Assignee = assignee
+            };
+            Array.Resize(ref todoItems, todoItems.Length + 1);
+            todoItems[^1] = newTodo;
+            return newTodo;
+        }
+
+        public void Clear()
+        {
+            todoItems = new Todo[0];
+        }
+    }
+}
