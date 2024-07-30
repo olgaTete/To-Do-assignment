cbd397e93ecc9dd3d0746614c25b279db36961ac
Olga Te <159808358+olgaTete@users.noreply.github.com>
2024-07-30 09:07:17 +0200

    add PeopleController.cs and TodoController.cs

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Console_core.Controllers
+{
+    public class TodoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
